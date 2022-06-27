8e26b19edccc6f30f6d08d331782b567f7b23b3b
sabrina silva <102616948+sabrina702@users.noreply.github.com>
2022-06-27 20:05:37 -0300

    Update exercicio 7.c

--- exercicio 7.c
+++ exercicio 7.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-
-int main(){
-	int atual=1,ant=1,cont,res,quant;
-	printf("Informe ate que termo quer ver: ");
-	scanf("%d",&quant);
-	printf("%d\n",ant);
-	res = atual;
-	for(cont=1;res<=quant;cont++){
-		printf("%d\n",res);
-		res = atual + ant;
-		ant = atual;
-		atual = res;
-	}
-	return 0;	
+programa
+{
+	
+	funcao inicio(){
+	
+	inteiro atual=1,ant=1,cont,res,quant
+	escreva("Informe ate que termo quer ver: ")
+	leia(quant)
+	escreva(ant)
+	res = atual
+	para(cont=1;res<=quant;cont++){
+		escreva("%d\n",res)
+		res = atual + ant
+		ant = atual
+		atual = res
+	}	
 }
